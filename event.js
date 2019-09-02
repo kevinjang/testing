985b228e6822a4b4ca9f61fd4ed8db7843cdfad7
kevinjang <kevinjang@163.com>
2019-09-02 23:05:12 +0800

    Initial commit

---
+++ event.js
@@ -0,0 +1,24 @@
+// var EventEmitter = require('events').EventEmitter;
+// var event = new EventEmitter();
+
+// event.on('some_event',function(){
+//     console.log('some_event事件触发');
+// });
+// setTimeout(function(){
+//     event.emit('some_event');
+// }, 1000);
+
+// ---------------------------- the above is part 1
+
+var events = require('events');
+var emitter = new events.EventEmitter();
+emitter.on('someEvent',function(arg1, arg2){
+    console.log('listener1', arg1, arg2);
+});
+emitter.on('someEvent',function(arg1, arg2){
+    console.log('listener2', arg1, arg2);
+
+    console.log(emitter.listenerCount('someEvent'));
+});
+
+emitter.emit('someEvent', 'arg1参数', 'arg2参数');
